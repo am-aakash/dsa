f53b16472ed3f38c207286cdd631f3809d482796
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-01-24 16:55:47 +0530

    Create findMinInRotatedArray.java

---
+++ leetcode/binarySearch/findMinInRotatedArray.java
@@ -0,0 +1,13 @@
+int findMin(int[] num) {
+    int low = 0;
+    int high = num.length - 1;
+    while(low < high){
+        int mid = (low + high) / 2;
+        if(num[high] < num[mid]){
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return num[high];
+}
