3b3d705d681c5dcaa6865cfa503420bbeeb861a0
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-03-15 10:59:39 +0530

    Create coinChange.cpp

---
+++ leetcode/dp/coinChange.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=0;i<amount+1;i++){
+            for(auto coin: coins){
+                if(coin<=i) dp[i]=min(dp[i], 1+dp[i-coin]);
+            }
+        }
+        return dp[amount]>amount? -1:dp[amount];
+    }
+};
