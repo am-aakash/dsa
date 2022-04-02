63ee5e61677620259c1b893346594c4acfa4c9e7
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-04-02 23:55:02 +0530

    Create 1650B.cpp

---
+++ codeforces/maths | arrays | greedy | twoPointer | sorting | hashing/1650B.cpp
@@ -0,0 +1,58 @@
+/* I can do this all day */
+// @am_aakash 
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#define SPEED ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define int long long
+#define REP(i,n)        for (int i = 0; i < n; i++)
+#define all(v)          v.begin(), v.end()
+    //    //    //  //  STL
+#define ii              pair<int,int>
+#define vi              vector<int>
+#define vii             vector<ii>
+#define vvi             vector<vi>
+#define mpii            map<int,int>
+#define min_heap        priority_queue<int,vector<int>,greater<int>>
+    //    //    //   // STL ALGORITHMS
+#define CNT(arr,n)      count(all(arr),n)
+#define CNT_IF(arr,iff) count_if(all(arr),iff)
+#define LB(arr,n)       lower_bound(all(arr),n)-arr.begin()
+#define UB(arr,n)       upper_bound(all(arr),n)-arr.begin()
+#define Min(arr)        min_element(all(arr))
+#define Max(arr)        max_element(all(arr))
+
+#define pb              push_back
+#define ff              first
+#define ss              second
+
+typedef unsigned long long ll;
+#define INF 1000000
+#define M109 1000000007
+using namespace std;
+
+
+void solve() {
+    // int n;cin>>n;
+    // string s; cin>>s;
+    // vi a(n);
+    // for(auto& it:a) cin>>it;
+    // sort(all(a),greater<int>());
+    int l,r,x; cin>>l>>r>>x;
+    int ans = r / x + r % x;
+    int m = r / x * x - 1;
+    if(m >= l)ans = max(ans, m / x + m % x);
+    cout << ans<< "\n";
+}
+
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+    freopen("input2.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    SPEED;
+
+    int T=1;
+    cin>>T;
+    while (T--) solve();
+    return 0;
+}
