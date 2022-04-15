a3f6da41e26222873fbed4007fb74fcde29ca968
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-04-15 14:25:52 +0530

    Create trimBST.cpp

---
+++ leetcode/tree/trimBST.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return NULL;
+        
+        if(root->val > high) root=root->left;
+        else if(root->val < low) root=root->right;
+        else{
+            root->left = trimBST(root->left,low,high);
+            root->right = trimBST(root->right,low,high);
+            return root;
+        }
+        return trimBST(root,low,high);
+    }
+};
