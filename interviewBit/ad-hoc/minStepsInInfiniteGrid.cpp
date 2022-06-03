67d57c3a51488a854eb81c15fa64bbc997c260e2
am-aakash <akashchaudhary12121@gmail.com>
2022-06-03 16:18:47 +0530

    minStepsInInfiniteGrid Added

---
+++ interviewBit/ad-hoc/minStepsInInfiniteGrid.cpp
@@ -0,0 +1,9 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B)
+{
+    int n = A.size(), ans = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        ans += max(abs(A[i] - A[i + 1]), abs(B[i] - B[i + 1]));
+    }
+    return ans;
+}
