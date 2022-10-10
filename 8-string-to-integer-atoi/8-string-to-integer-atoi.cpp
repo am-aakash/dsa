5eb1a11f1576b8e760f326e90fe9074fdf0a7c7d
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-10-10 07:32:42 +0530

    Time: 3 ms (85.76%), Space: 7 MB (88.41%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        int sign =1;
+        long int result=0;
+        int i;
+        int s=str.size();
+        
+        // checking for spaces
+        for(i=0;i<s;i++){
+            if (i>s || i==s) return 0;
+            if(str[i]!=' ')
+                break;
+        }
+        // setting the sign
+        if(str[i]=='+'||str[i]=='-') sign= str[i++]=='-' ?-1:1;
+        
+        // getting the next numbers
+        while(i<s && isdigit(str[i])){
+            result= result*10 + (str[i++]-'0');
+            if(result*sign >= INT_MAX) return INT_MAX;
+            if(result*sign <= INT_MIN) return INT_MIN;
+        } 
+        
+        return result*sign;
+    }
+};
