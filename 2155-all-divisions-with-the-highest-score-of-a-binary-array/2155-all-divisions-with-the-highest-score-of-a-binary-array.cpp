bb6b226295471b2854172c56d5c68420b9d49ce0
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-10-19 11:29:51 +0530

    Time: 560 ms (77.26%), Space: 286.7 MB (63.54%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     vector<int> maxScoreIndices(vector<int>& arr) {
         int n = arr.size();
-        map<int, vector<int>> mpp;
+        for(int i=1; i<n; i++) arr[i] += arr[i-1];
         
-        for(int i=1; i<n; i++){
-            arr[i] += arr[i-1];
-        }
+        vector<int> res;
         int highest = arr[n-1];
-        mpp[highest].push_back(0);
+        res.push_back(0);
         for(int i=1; i<=n; i++){
             int left = i-arr[i-1], right = arr[n-1]-arr[i-1];
-            if(left+right > highest) highest = left+right;
-            mpp[left+right].push_back(i);
+            if(left+right == highest) res.push_back(i);
+            else if(left+right > highest){
+                highest = left+right;
+                res.clear();
+                res.push_back(i);
+            }
         }
-        return mpp[highest];
+        return res;
     }
 };
