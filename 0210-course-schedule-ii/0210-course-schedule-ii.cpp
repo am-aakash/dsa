824c1f56e625fc96eb853abea01da8f1e696f25e
Aakash Chaudhary <64517711+am-aakash@users.noreply.github.com>
2022-10-26 16:43:42 +0530

    Time: 43 ms (57.89%), Space: 13.9 MB (65.20%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    vector<int> indegreeHelper(int N, vector<int> adj[]){
+        vector<int> indegree(N, 0);
+        for(int i=0; i<N; i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
+            }
+        }
+        return indegree;
+    }
+public:
+    vector<int> kahnsAlgorithm(int N, vector<int> adj[]){
+        vector<int> indegree = indegreeHelper(N, adj);
+        
+        queue<int> q;
+        for(int i=0; i<N; i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        
+        vector<int> topoSort;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topoSort.push_back(node);
+            
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(!indegree[it]) q.push(it);
+            }
+        }
+        return topoSort;
+    }
+    vector<int> findOrder(int N, vector<vector<int>>& prerequisites) {
+        vector<int> adj[N];
+	    for(auto p : prerequisites){
+            adj[p[0]].push_back(p[1]);
+        }
+	    
+	    vector<int> topoSort = kahnsAlgorithm(N, adj);
+        reverse(topoSort.begin(), topoSort.end());
+        if(topoSort.size() < N) return {};
+        return topoSort;
+    }
+};
